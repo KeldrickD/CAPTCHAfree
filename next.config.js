c00b4167a30683b904fe3a16226c887ef824d9ad
Your Name <you@example.com>
2025-05-08 06:00:19 -0400

    fix: configure Tailwind CSS and update Next.js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  experimental: {
-    optimizeFonts: true,
-  },
   images: {
     domains: ['captchafree.vercel.app'],
   },
